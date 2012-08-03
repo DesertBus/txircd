3d4c5329ae0f947888d1e86205ef8254e5ffb27b
Fugiman <Fugiman47@gmail.com>
2012-08-03 14:04:05 -0400

    IRCServer is "done"
    Delete everything that isn't immediately useful
    Delegate handleCommand to other classes

--- txircd/ircd.py
+++ txircd/ircd.py
@@ -1,223 +1,84 @@
 # -*- coding: utf-8 -*-
-import time
-from functools import partial
 from twisted.internet import reactor
 from twisted.internet.protocol import Factory
 from twisted.python import log
 from twisted.words.protocols import irc
 from txircd.utils import CaseInsensitiveDictionary, DefaultCaseInsensitiveDictionary
-from .utils import parse_args, multi, iterate_non_blocking
-
+from txircd.server import IRCServer
+from txircd.service import IRCService
+from txircd.user import IRCUser
+import uuid
 
 class IRCProtocol(irc.IRC):
-    UNREGISTERED_COMMANDS = ['PASS', 'USER', 'NICK', 'PING', 'QUIT']
-    REGISTERED_ATTRIBUTES = ['nick', 'username', 'hostname', 'servername', 'realname']
+    UNREGISTERED_COMMANDS = ['PASS', 'USER', 'SERVICE', 'SERVER', 'NICK', 'PING', 'QUIT']
 
     def __init__(self, *args, **kwargs):
+        self.type = None
+        self.password = None
         self.nick = None
-        self.username = None
-        self.hostname = None
-        self.servername = None
-        self.realname = None
-        self.sign_on = time.time()
-        self.last_msg = time.time()
-        self.channels = []
 
     def get_prefix(self):
         # FIXME: this is bugged! irssi does not recognize stuff sent back as coming from itself
         return '%s!%s@%s' % (self.nick, self.username, self.transport.getHandle().getpeername()[0])
 
-    def broadcast_message(self, command, *parameter_list):
-        return self.ircd.broadcast_message(command, *parameter_list, prefix=self.get_prefix())
-
-    def initiate_timeout(self):
-        self.timeout = reactor.callLater(self.ircd.client_timeout, self.handle_timeout)
-
-    def handle_timeout(self):
-        if self.nick:
-            log.msg("Client %r timed out!" % self.nick)
-            self.ircd.quit(self)
-            self.sendMessage('KILL', 'Timeout')
-            self.transport.loseConnection()
-
-    def is_registered(self):
-        get_from_self = partial(getattr, self)
-        return all(map(get_from_self, self.REGISTERED_ATTRIBUTES))
-
-    def check_registration(self):
-        if self.is_registered():
-            if self.ircd.join_server(self):     
-                log.msg("User %r joined server, %s users on server" % (self.nick, len(self.ircd.users)))
-                self.sendMessage('MODE')
-                self.broadcast_message('NICK', self.nick)
-
     def handleCommand(self, command, prefix, params):
         log.msg('handleCommand: %r %r %r' % (command, prefix, params))
-        self.timeout.reset(self.ircd.client_timeout)
-        if not self.is_registered() and command not in self.UNREGISTERED_COMMANDS:
+        if not self.type and command not in self.UNREGISTERED_COMMANDS:
             return self.sendMessage(irc.ERR_NOTREGISTERED)
-        else:
+        elif not self.type:
             return irc.IRC.handleCommand(self, command, prefix, params)
+        else:
+            return irc.IRC.handleCommand(self.type, command, prefix, params)
 
     def sendLine(self, line):
         log.msg('sendLine: %r' % line)
         return irc.IRC.sendLine(self, line)
 
-    def irc_unknown(self, command, params):
+    def irc_PASS(self, prefix, params):
+        self.password = params
+
+    def irc_NICK(self, prefix, params):
+        self.nick = params
+
+    def irc_USER(self, prefix, params):
+        self.type = IRCUser(self, params, self.password, self.nick)
+
+    def irc_SERVICE(self, prefix, params):
+        self.type = IRCService(self, params, self.password)
+
+    def irc_SERVER(self, prefix, params):
+        self.type = IRCServer(self, params, self.password)
+
+    def irc_PING(self, prefix, params):
         pass
 
-    @parse_args
-    def irc_USER(self, prefix, username, hostname, servername, realname):
-        self.sendLine(irc.RPL_WELCOME)
-        self.username = username
-        self.hostname = hostname
-        self.servername = servername
-        self.realname = realname
-        self.check_registration()
-
-    @parse_args
-    def irc_NICK(self, prefix, nickname=None, hopcount=None):
-        """
-        /*
-        ** 'do_nick_name' ensures that the given parameter (nick) is
-        ** really a proper string for a nickname (note, the 'nick'
-        ** may be modified in the process...)
-        **
-        **	RETURNS the length of the final NICKNAME (0, if
-        **	nickname is illegal)
-        **
-        **  Nickname characters are in range
-        **	'A'..'}', '_', '-', '0'..'9'
-        **  anything outside the above set will terminate nickname.
-        **  In addition, the first character cannot be '-' or a digit.
-        **  Finally forbid the use of "anonymous" because of possible
-        **  abuses related to anonymous channnels. -kalt
-        **
-        */
-        """
-        log.msg("NICK: %r %r" % (nickname, hopcount))
-        if not nickname:
-            return self.sendMessage(irc.ERR_NONICKNAMEGIVEN)
-        elif nickname in self.ircd.users:
-            return self.sendMessage(irc.ERR_NICKNAMEINUSE)
-        if self.nick:
-            self.ircd.rename(self.nick, nickname)
-        self.nick = nickname
-        self.check_registration()
-        self.sendMessage(irc.RPL_WELCOME, self.nick, self.ircd.welcome_message, prefix=self.ircd.name)
-
-    @parse_args
-    def irc_PRIVMSG(self, prefix, targets, message):
-        self.last_msg = time.time()
-
-        def itr():
-            for target in targets.split(','):
-                for _ in self.ircd.privmsg(self.get_prefix(), target, message):
-                    yield
-
-        iterate_non_blocking(itr())
-
-    @parse_args
-    def irc_PING(self, prefix, *servers):
-        self.sendLine('PONG')
-
-    @parse_args
-    def irc_MODE(self, prefix, *args):
-        pass
-
-    def server_WHOIS(self, prefix, server, nicknames):
-        log.msg("server_WHOIS %r %r" % (server, nicknames))
-
-    def user_WHOIS(self, prefix, nicknames):
-        log.msg("user_WHOIS %r" % nicknames)
-        def itr():
-            for nickname in nicknames.split(','):
-                client = self.ircd.get_client(nickname)
-                if client is None:
-                    self.sendMessage(irc.ERR_NOSUCHNICK)
-                else:
-                    self.whois(self.nick, client.nick, client.username, client.hostname, client.realname, 'localhost',
-                        'localhost', False, time.time() - client.last_msg, client.sign_on, client.channels)
-                yield
-        iterate_non_blocking(itr())
-
-    irc_WHOIS = multi('irc_WHOIS', server_WHOIS, user_WHOIS)
-
-    @parse_args
-    def irc_JOIN(self, prefix, channel, keys=None):
-        self.channels.append(channel)
-        self.ircd.join_channel(self, channel)
-        self.sendMessage(irc.RPL_TOPIC, "No topic")
-
-    @parse_args
-    def irc_QUIT(self, prefix, message):
-        # FIXME: seems bugged. irssi hangs too long on quit for this to work reliably.
-        if self.nick:
-            self.broadcast_message('QUIT', message)
-            self.ircd.leave_server(self)
-
+    def irc_QUIT(self, prefix, params):
+        self.loseConnection()
 
 class IRCD(Factory):
     protocol = IRCProtocol
 
-    def __init__(self, name, client_timeout=5 * 60, welcome_message="Welcome to TXIRCd"):
+    def __init__(self, name, client_timeout=5 * 60, description="Welcome to TXIRCd"):
         self.name = name
-        self.welcome_message = welcome_message
+        self.token = uuid.uuid1()
+        self.description = description
         self.client_timeout = client_timeout
-        self.channels = DefaultCaseInsensitiveDictionary(list)
+        self.servers = CaseInsensitiveDictionary()
         self.users = CaseInsensitiveDictionary()
+        self.channels = DefaultCaseInsensitiveDictionary(self.createChannel)
 
-    def get_client(self, nick):
-        return self.users.get(nick, None)
+    def createChannel(self, name):
+        return {
+            "name": name,
+            "mode": "",
+            "password": None,
+            "limit": None,
+            "users": CaseInsensitiveDictionary(),
+            "bans": [],
+            "exemptions": [],
+            "invites": []
+        }
 
-    def join_channel(self, client, channel):
-        if client not in self.channels[channel]:
-            self.channels[channel].append(client)
-            for client in self.channels[channel]:
-                client.sendMessage('JOIN', channel, prefix=client.get_prefix())
-            log.msg("%s joined channel %s, %s users in channel" % (client.nick, channel, len(self.channels[channel])))
-
-    def leave_channel(self, client, channel):
-        if client in self.channels[channel]:
-            self.channels[channel].remove(client.nick)
-        if channel in self.users[client.nick]:
-            self.users[client.nick]['channels'].remove(channel)
-
-    def join_server(self, client):
-        if client.nick in self.users:
-            return False
-        self.users[client.nick] = client
-        return True
-
-    def leave_server(self, client):
-        for channel in client.channels:
-            if client in self.channels[channel]:
-                self.channels[channel].remove(client)
-        if client.nick in self.users:
-            del self.users[client.nick]
-        log.msg("User %r quit server, %s on server" % (client.nick, len(self.users)))
-
-    def privmsg(self, sender, target, message):
-        if target.startswith('#'):
-            for client in self.channels[target]:
-                client.privmsg(sender, target, message)
-                yield
-        else:
-            client = self.users.get(target, None)
-            if client:
-                client.privmsg(sender, target, message)
-            yield
-
-    def rename(self, old, new):
-        if old in self.users:
-            self.users[new] = self.users.pop(old)
-
-    def broadcast_message(self, command, *parameter_list, **prefix):
-        for client in self.users.values():
-            client.sendMessage(command, *parameter_list, **prefix)
-
-    def buildProtocol(self, addr):
-        proto = Factory.buildProtocol(self, addr)
-        proto.ircd = self
-        proto.initiate_timeout()
-        return proto
+    def broadcast(channel, message):
+        for u in self.channels[channel].users.iterkeys():
+            self.users[u].socket.sendLine(message)
--- txircd/utils.py
+++ txircd/utils.py
@@ -1,106 +1,6 @@
 # -*- coding: utf-8 -*-
 from collections import MutableMapping
-from functools import wraps
-import inspect
 from twisted.internet import reactor
-from twisted.python import log
-from twisted.words.protocols import irc
-
-
-class ParamParserError(Exception):
-    pass
-
-
-class NeedMoreParams(ParamParserError):
-    pass
-
-
-class TooManyParams(ParamParserError):
-    pass
-
-
-class ParamParser(object):
-    def __init__(self, method):
-        #todo: cleanup
-        args, varargs, varkw, defaults = inspect.getargspec(method)
-        if varkw:
-            raise TypeError("Methods decorated with parse_args or multi may not use **kwargs")
-        if args[0] != 'self':
-            raise TypeError("Methods decorated with parse_args or multi must have 'self' as first argument")
-        if args[1] != 'prefix':
-            raise TypeError("Methods decorated with parse_args or multi must have 'prefix' as second argument")
-        del args[:2] # self, prefix
-
-        # amount of non-var-args
-        fix_args = len(args)
-        # amount of defaults
-        num_def = len(defaults) if defaults else 0
-        # minimum amount of arguments
-        min_args = fix_args - num_def
-        # maximum amount of arguments or -1 if varags or varkw is used
-        max_args = -1 if varargs else len(args)
-
-        # dictionary of default values
-        if defaults:
-            default_args = dict(zip(args[-num_def:], defaults))
-        else:
-            default_args = {}
-
-        self.method = method
-        self.argnames = args
-        self.fix_args = fix_args
-        self.min_args = min_args
-        self.max_args = max_args
-        self.default_args = default_args
-
-    def parse(self, params):
-        # check arg count
-        arg_count = len(params)
-        if arg_count < self.min_args:
-            raise NeedMoreParams("Got %s params, expected a minimum of %s" % (arg_count, self.min_args))
-        elif 0 < self.max_args < arg_count:
-            raise TooManyParams("Got %s params, expected a maximum of %s" % (arg_count, self.max_args))
-        else:
-            kwargs = {}
-            kwargs.update(self.default_args)
-            kwargs.update(zip(self.argnames, params))
-            rest = params[self.fix_args:]
-            return rest, kwargs
-
-
-def parse_args(method):
-    parser = ParamParser(method)
-
-    @wraps(method)
-    def wrapper(self, prefix, params):
-        try:
-            rest, kwargs = parser.parse(params)
-        except NeedMoreParams:
-            return self.sendLine(irc.ERR_NEEDMOREPARAMS)
-        except TooManyParams:
-            return self.sendLine(irc.ERR_NEEDMOREPARAMS) # todo: is this the best response?
-        log.msg("Calling %r with %r *%r **%r" % (method.__name__, prefix, rest, kwargs))
-        return method(self, prefix, *rest, **kwargs)
-
-    return wrapper
-
-
-def multi(name, *methods):
-    parsers = [ParamParser(method) for method in methods]
-
-    def wrapper(self, prefix, params):
-        for parser in parsers:
-            try:
-                rest, kwargs = parser.parse(params)
-            except ParamParserError:
-                continue
-            log.msg("Calling %r with %r *%r **%r" % (parser.method.__name__, prefix, rest, kwargs))
-            return parser.method(self, prefix, *rest, **kwargs)
-        return self.sendLine(irc.ERR_NEEDMOREPARAMS) # todo: just handle it like this?
-
-    wrapper.__name__ = name
-    return wrapper
-
 
 def iterate_non_blocking(iterator):
     try:
@@ -109,6 +9,8 @@ def iterate_non_blocking(iterator):
         return
     reactor.callLater(0, iterate_non_blocking, iterator)
 
+def irc_lower(str):
+    return str.lower().replace('[','{').replace(']','}').replace('/','|').replace('~','^')
 
 class CaseInsensitiveDictionary(MutableMapping):
     def __init__(self):
@@ -119,13 +21,13 @@ class CaseInsensitiveDictionary(MutableMapping):
 
     def __delitem__(self, key):
         try:
-            del self._data[key.lower()]
+            del self._data[irc_lower(key)]
         except KeyError:
             raise KeyError(key)
 
     def __getitem__(self, key):
         try:
-            return self._data[key.lower()]
+            return self._data[irc_lower(key)]
         except KeyError:
             raise KeyError(key)
 
@@ -136,7 +38,7 @@ class CaseInsensitiveDictionary(MutableMapping):
         return len(self._data)
 
     def __setitem__(self, key, value):
-        self._data[key.lower()] = value
+        self._data[irc_lower(key)] = value
 
 
 class DefaultCaseInsensitiveDictionary(CaseInsensitiveDictionary):
@@ -148,5 +50,5 @@ class DefaultCaseInsensitiveDictionary(CaseInsensitiveDictionary):
         try:
             return super(DefaultCaseInsensitiveDictionary, self).__getitem__(key)
         except KeyError:
-            value = self[key] = self._default_factory()
+            value = self[key] = self._default_factory(key)
             return value
