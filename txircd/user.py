ae4a0c9bcd8cd2b8babb33d89dbd151a5593b252
ElementalAlchemist <ElementAlchemist7@gmail.com>
2012-09-16 15:09:24 -0400

    Do permissions checking on the topic

--- txircd/user.py
+++ txircd/user.py
@@ -175,14 +175,19 @@ class IRCUser(object):
             if cdata["topic"]["message"] is not None:
                 self.parent.topicAuthor(self.data["nickname"], cdata["name"], cdata["topic"]["author"], cdata["topic"]["created"])
         else:
-            # TODO: check user's channel status if 't' in cdata["modes"]
-            cdata["topic"] = {
-                "message": params[1],
-                "author": self.data["nickname"],
-                "created": time.time()
-            }
-            for u in cdata["users"].iterkeys():
-                self.parent.factory.users[u]["socket"].topic(self.parent.factory.users[u]["nickname"], cdata["name"], params[1], self.prefix())
+            if self.data["nickname"] not in cdata["users"]:
+                self.parent.sendMessage(irc.ERR_NOTONCHANNEL, "%s %s :You're not in that channel" % (self.data["nickname"], cdata["name"]), prefix=self.parent.hostname)
+            elif 't' in cdata["modes"] and cdata["users"][self.data["nickname"]] and self.parent.factory.PREFIX_ORDER.find(cdata["users"][self.data["nickname"]][0]) <= self.parent.factory.PREFIX_ORDER.find('h'):
+                # If the channel is +t and the user has a rank that is halfop or higher, allow the topic change
+                cdata["topic"] = {
+                    "message": params[1],
+                    "author": self.data["nickname"],
+                    "created": time.time()
+                }
+                for u in cdata["users"].iterkeys():
+                    self.parent.factory.users[u]["socket"].topic(self.parent.factory.users[u]["nickname"], cdata["name"], params[1], self.prefix())
+            else:
+                self.parent.sendMessage(irc.ERR_CHANOPRIVSNEEDED, "%s %s :You do not have access to change the topic on this channel" % (self.data["nickname"], cdata["name"]), prefix=self.parent.hostname)
 
     def irc_WHO(self, prefix, params):
         pass
