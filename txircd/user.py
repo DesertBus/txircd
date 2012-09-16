72160eb5cbea1ce98717b66ecfb245805d56ffb4
ElementalAlchemist <ElementAlchemist7@gmail.com>
2012-09-16 14:39:33 -0400

    Implement the TOPIC command

--- txircd/user.py
+++ txircd/user.py
@@ -161,6 +161,28 @@ class IRCUser(object):
     
     def irc_MODE(self, prefix, params):
         pass
+    
+    def irc_TOPIC(self, prefix, params):
+        if not params:
+            self.parent.sendMessage(irc.ERR_NEEDMOREPARAMS, "%s TOPIC :Not enough parameters" % self.data["nickname"], prefix=self.parent.hostname)
+            return
+        if params[0] not in self.parent.factory.channels:
+            self.parent.sendMessage(irc.ERR_NOSUCHCHANNEL, "%s %s :No such channel" % (self.data["nickname"], params[0]), prefix=self.parent.hostname)
+            return
+        cdata = self.parent.factory.channels[params[0]]
+        if len(params) == 1:
+            self.parent.topic(self.data["nickname"], cdata["name"], cdata["topic"]["message"])
+            if cdata["topic"]["message"] is not None:
+                self.parent.topicAuthor(self.data["nickname"], cdata["name"], cdata["topic"]["author"], cdata["topic"]["created"])
+        else:
+            # TODO: check user's channel status if 't' in cdata["modes"]
+            cdata["topic"] = {
+                "message": params[1],
+                "author": self.data["nickname"],
+                "created": time.time()
+            }
+            for u in cdata["users"].iterkeys():
+                self.parent.factory.users[u]["socket"].topic(self.parent.factory.users[u]["nickname"], cdata["name"], params[1], self.prefix())
 
     def irc_WHO(self, prefix, params):
         pass
