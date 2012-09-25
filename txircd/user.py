98f3b3b0d36f4c0e9b09d5736e183629d5b5289f
Fugiman <Fugiman47@gmail.com>
2012-09-25 17:52:20 -0400

    Fix TOPIC

--- txircd/user.py
+++ txircd/user.py
@@ -300,7 +300,7 @@ class IRCUser(object):
         else:
             if self.nickname not in cdata["users"]:
                 self.socket.sendMessage(irc.ERR_NOTONCHANNEL, "%s %s :You're not in that channel" % (self.nickname, cdata["name"]), prefix=self.socket.hostname)
-            elif 't' not in cdata["mode"] or self.hasAccess(params[0],"h"):
+            elif not cdata["mode"].has("t") or self.hasAccess(params[0],"h"):
                 # If the channel is +t and the user has a rank that is halfop or higher, allow the topic change
                 cdata["topic"] = {
                     "message": params[1],
