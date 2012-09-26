dacbfa94b158371eb3066a29cc6b26c1721b6dbc
ElementalAlchemist <ElementAlchemist7@gmail.com>
2012-09-25 21:24:59 -0400

    Fix derp in status remover on KICK

--- txircd/user.py
+++ txircd/user.py
@@ -360,8 +360,8 @@ class IRCUser(object):
             del self.ircd.channels[params[0]] # destroy the empty channel
         else:
             for rank in self.ircd.prefix_order:
-                if self.nickname in cdata["mode"].get(rank):
-                    cdata["mode"].combine("-%s" % rank, [self.nickname], self.nickname)
+                if udata.nickname in cdata["mode"].get(rank):
+                    cdata["mode"].combine("-%s" % rank, [udata.nickname], self.nickname)
 
     def irc_WHO(self, prefix, params):
         pass
