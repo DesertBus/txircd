65a306fe3a4ffb96ef63b92f0a8a03d19a0e0da0
ElementalAlchemist <ElementAlchemist7@gmail.com>
2012-10-07 18:25:34 -0400

    Also apply to NOTICE and status messages

--- txircd/user.py
+++ txircd/user.py
@@ -731,6 +731,8 @@ class IRCUser(object):
                 return self.socket.sendMessage(irc.ERR_NOSUCHNICK, self.nickname, target, ":No such nick/channel", prefix=self.ircd.hostname) # I don't know what they were trying to send to.
             min_status = symbol_prefix[target[0]]
             c = self.ircd.channels[target[1:]]
+            if c.mode.has("S") and (not self.hasAccess(c.name, "h") or "S" not in self.ircd.exempt_chanops):
+                message = strip_colors(message)
             for u in c.users.itervalues():
                 if u.nickname is not self.nickname and u.hasAccess(c.name, min_status):
                     u.socket.sendMessage("PRIVMSG", "{}{}".format(target[0], c.name), ":{}".format(message), prefix=self.prefix())
@@ -756,6 +758,8 @@ class IRCUser(object):
                 return self.socket.sendMessage(irc.ERR_CANNOTSENDTOCHAN, self.nickname, c.name, ":Cannot send to channel (+m)", prefix=self.ircd.hostname)
             if self.channels[c.name]["banned"] and not (self.channels[c.name]["exempt"] or self.mode.has("o") or self.hasAccess(c.name, "v")):
                 return self.socket.sendMessage(irc.ERR_CANNOTSENDTOCHAN, self.nickname, c.name, ":Cannot send to channel (banned)", prefix=self.ircd.hostname)
+            if c.mode.has("S") and (not self.hasAccess(c.name, "h") or "S" not in self.ircd.exempt_chanops):
+                message = strip_colors(message)
             for u in c.users.itervalues():
                 if u.nickname is not self.nickname:
                     u.socket.sendMessage("NOTICE", c.name, ":{}".format(message), prefix=self.prefix())
@@ -766,6 +770,8 @@ class IRCUser(object):
                 return self.socket.sendMessage(irc.ERR_NOSUCHNICK, self.nickname, target, ":No such nick/channel", prefix=self.ircd.hostname)
             min_status = symbol_prefix[target[0]]
             c = self.ircd.channels[target[1:]]
+            if c.mode.has("S") and (not self.hasAccess(c.name, "h") or "S" not in self.ircd.exempt_chanops):
+                message = strip_colors(message)
             for u in c.users.itervalues():
                 if u.nickname is not self.nickname and u.hasAccess(c.name, min_status):
                     u.socket.sendMessage("NOTICE", "{}{}".format(target[0], c.name), ":{}".format(message), prefix=self.prefix())
