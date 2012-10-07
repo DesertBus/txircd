0b8a12937bd0d4b20b58257abe968ad4ad9d4575
ElementalAlchemist <ElementAlchemist7@gmail.com>
2012-10-07 18:24:16 -0400

    Add exempt_chanops to list of config vars read

--- txircd/ircd.py
+++ txircd/ircd.py
@@ -212,7 +212,7 @@ class IRCD(Factory):
             "oper_hosts","opers","vhosts","log_dir","max_data","maxConnectionsPerPeer",
             "maxConnectionExempt","ping_interval","timeout_delay","ban_msg",
             "db_library","db_marker","db_username","db_password","db_database",
-            "nickserv_timeout","nickserv_limit","nickserv_guest_prefix"]
+            "nickserv_timeout","nickserv_limit","nickserv_guest_prefix","exempt_chanops"]
         self.version = "0.1"
         self.created = now()
         self.token = uuid.uuid1()
