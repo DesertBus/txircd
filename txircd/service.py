f4353c26a99d4b176a084fdfc3b4d4be16b7af62
ElementalAlchemist <ElementAlchemist7@gmail.com>
2012-11-09 10:12:43 -0500

    Remove unused files

--- txircd/service.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-class IRCService(object):
-    def __init__(self, parent, service, password):
-        pass
-
-    def irc_unknown(self, prefix, command, params):
-        raise NotImplementedError(command, prefix, params)
