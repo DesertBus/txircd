b4538afcd31182dafdf120a45b3e084a7a4fc820
ElementalAlchemist <ElementAlchemist7@gmail.com>
2012-11-09 10:44:26 -0500

    Add module base classes; see file for rationale

---
+++ txircd/modbase.py
@@ -0,0 +1,50 @@
+# The purpose of this file is to provide base classes with the needed functions
+# already defined; this allows us to guarantee that any exceptions raised
+# during function calls are a problem with the module and not just that the
+# particular function isn't defined.
+
+class Module(object):
+	def onJoinCheck(self, channel, user):
+		pass
+	def onJoinComplete(self, channel, user):
+		pass
+	def onMessage(self, msgType, sender, target, message):
+		pass
+	def onPart(self, channel, user, reason):
+		pass
+	def onTopicChange(self, channel, user, topic):
+		pass
+	def onConnect(self, ip):
+		pass
+	def onRegister(self, user):
+		pass
+	def onQuit(self, user, reason):
+		pass
+	def onCommandExtra(self, command, params):
+		pass
+	def onMetadataUpdate(self, user, key, oldvalue, newvalue):
+		pass
+	def onRecvData(self, data):
+		pass
+	def onSendData(self, data):
+		pass
+
+class Mode(object):
+	def checkSet(self, channel, param):
+		pass
+	def checkUnset(self, channel, param):
+		pass
+	def onJoin(self, channel, user, params):
+		pass
+	def onMessage(self, sender, target, message):
+		pass
+	def onPart(self, channel, user, reason):
+		pass
+	def onTopicChange(self, channel, user, topic):
+		pass
+	def commandData(self, command, *args):
+		pass
+
+def Command(object):
+	def onUse(self, user, params):
+		pass
