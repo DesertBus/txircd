8bf0a6a092aff9fa4ff221f3eed23360d46d0369
ElementalAlchemist <ElementAlchemist7@gmail.com>
2012-11-26 22:00:02 -0500

    Have the NICK command take advantage of the nick helper function and the processParams function

--- txircd/modules/cmd_nick.py
+++ txircd/modules/cmd_nick.py
@@ -3,7 +3,7 @@ from txircd.modbase import Command
 from txircd.utils import VALID_USERNAME, irc_lower
 
 class NickCommand(command):
-	def onUse(self, user, params):
+	def onUse(self, user, data):
 		if not params:
 			user.sendMessage(irc.ERR_NONICKNAMEGIVEN, ":No nickname given")
 			return
@@ -18,27 +18,34 @@ class NickCommand(command):
 			return
 		if params[0] == user.nickname:
 			return # do nothing when the given nick is the exact same as the user's current nick
-		# changing nicks now
 		if user.registered == 0:
-			del self.ircd.users[user.nickname]
-			self.ircd.users[params[0]] = user
-			notify = set()
-			notify.append(user)
-			for chan in user.channels.iterkeys():
-				cdata = self.ircd.channels[chan]
-				del cdata.users[user.nickname]
-				cdata.users[params[0]] = user
-				for cuser in cdata.users:
-					notify.append(cuser)
-			oldprefix = user.prefix()
-			user.nickname = params[0]
-			for u in notify:
-				u.sendMessage("NICK", to=params[0], prefix=oldprefix)
+			user.nick(params["nick"])
 		else:
-			user.nickname = params[0]
-			user.registered -= 1
+			if not user.nickname:
+				user.registered -= 1
+			user.nickname = params["nick"]
 			if user.registered == 0:
 				user.register()
+	
+	def processParams(self, user, params):
+		if not params:
+			user.sendMessage(irc.ERR_NONICKNAMEGIVEN, ":No nickname given")
+			return {}
+		if not params[0]:
+			user.sendMessage(irc.ERR_ERRONEUSNICKNAME, "*", ":Erroneous nickname")
+			return {}
+		if not VALID_USERNAME.match(params[0]):
+			user.sendMessage(irc.ERR_ERRONEUSNICKNAME, params[0], ":Erroneous nickname")
+			return {}
+		if params[0] == user.nickname:
+			return {}
+		if params[0] in self.ircd.users and irc_lower(params[0]) != irc_lower(user.nickname):
+			user.sendMessage(irc.ERR_NICKNAMEINUSE, self.ircd.users[params[0]].nickname, ":Nickname is already in use")
+			return {}
+		return {
+			"user": user,
+			"nick": params[0]
+		}
 
 def Spawner(object):
 	def __init__(self, ircd):
