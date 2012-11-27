27ee536137a98a317f2cfbb2010fa5fe31037e99
ElementalAlchemist <ElementAlchemist7@gmail.com>
2012-11-27 15:11:50 -0500

    Update the USER command to take advantage of core capabilities as well

--- txircd/modules/cmd_user.py
+++ txircd/modules/cmd_user.py
@@ -2,22 +2,30 @@ from twisted.words.protocols import irc
 from txircd.modbase import Command
 
 class UserCommand(Command):
-	def onUse(self, user, params):
-		if user.registered == 0:
-			self.sendMessage(irc.ERR_ALREADYREGISTRED, ":Unauthorized command (already registered)")
-			return
-		if params and len(params) < 4:
-			user.sendMessage(irc.ERR_NEEDMOREPARAMS, "USER", ":Not enough parameters")
+	def onUse(self, user, data):
 		if not user.username:
 			user.registered -= 1
-		user.username = filter(lambda x: x in string.ascii_letters + string.digits + "-_", params[0])[:12]
-		if not user.username:
-			user.registered += 1
-			user.sendMessage(irc.ERR_NEEDMOREPARAMS, "USER", ":Your username is not valid")
-			return
-		user.realname = params[3]
+		user.username = data["ident"]
+		user.realname = data["gecos"]
 		if user.registered == 0:
 			user.register()
+	
+	def processParams(self, user, params):
+		if user.registered == 0:
+			user.sendMessage(irc.ERR_ALREADYREGISTRED, ":Unauthorized command (already registered)")
+			return {}
+		if params and len(params) < 4:
+			user.sendMessage(irc.ERR_NEEDMOREPARAMS, "USER", ":Not enough parameters")
+			return {}
+		ident = filter(lambda x: x in string.ascii_letters + string.digits + "-_", params[0])[:12]
+		if not ident:
+			user.sendMessage(irc.ERR_NEEDMOREPARAMS, "USER", ":Your username is not valid")
+			return {}
+		return {
+			"user": user,
+			"ident": ident,
+			"gecos": params[3]
+		}
 
 def Spawner(object):
 	def __init__(self, ircd):
