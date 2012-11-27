1a30e85d33c026daf41bcfd56f05ed2fa0e5d48a
ElementalAlchemist <ElementAlchemist7@gmail.com>
2012-11-27 14:58:24 -0500

    Make the PASS command take advantage of processParams and handle the data dict correctly

--- txircd/modules/cmd_pass.py
+++ txircd/modules/cmd_pass.py
@@ -2,14 +2,20 @@ from twisted.words.protocols import irc
 from txircd.modbase import Command, Module
 
 class PassCommand(Command, Module):
-	def onUse(self, user, params):
+	def onUse(self, user, data):
+		user.password = data["password"]
+	
+	def processParams(self, user, params):
 		if user.registered == 0:
 			user.sendMessage(irc.ERR_ALREADYREGISTRED, ":Unauthorized command (already registered)")
-			return
+			return {}
 		if not params:
 			user.sendMessage(irc.ERR_NEEDMOREPARAMS, "PASS", ":Not enough parameters")
-			return
-		user.password = params[0]
+			return {}
+		return {
+			"user": user,
+			"password": params[0]
+		}
 	
 	def onRegister(self, user):
 		if self.ircd.server_password and self.ircd.server_password != user.password:
