c97ed07fe26ce2d8e15875d0b46fbd922129ab09
ElementalAlchemist <ElementAlchemist7@gmail.com>
2012-11-28 12:13:50 -0500

    Implement QUIT command

---
+++ txircd/modules/cmd_quit.py
@@ -0,0 +1,37 @@
+from twisted.words.protocols import irc
+from txircd.modbase import Command
+
+class QuitCommand(Command):
+	def onUse(self, user, data):
+		quit_to = set()
+		for chan in user.channels.iterkeys():
+			cdata = self.ircd.channels[chan]
+			self.leave(chan)
+			for u in cdata.users.itervalues():
+				quit_to.add(u)
+		for u in quit_to:
+			u.sendMessage("QUIT", ":{}".format(reason), to=None, prefix=user.prefix())
+		user.sendMessage("ERROR", ":Closing Link {} [{}]".format(user.prefix(), reason), to=None, prefix=None)
+		del self.ircd.users[user.nickname]
+		user.socket.transport.loseConnection()
+	
+	def processParams(self, user, params):
+		reason = params[0] if params and params[0] else "Client exited"
+		return {
+			"user": user,
+			"reason": reason
+		}
+
+class Spawner(object):
+	def __init__(self, ircd):
+		self.ircd = ircd
+	
+	def spawn():
+		return {
+			"commands": {
+				"QUIT": QuitCommand()
+			}
+		}
+	
+	def cleanup():
+		del self.ircd.commands["QUIT"]
