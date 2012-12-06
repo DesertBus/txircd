bf75aa766d511d7d4450cc5994a6f87839dfb477
ElementalAlchemist <ElementAlchemist7@gmail.com>
2012-12-06 15:25:04 -0500

    Make IRCChannel provide mode strings

--- txircd/channel.py
+++ txircd/channel.py
@@ -1,7 +1,8 @@
 from txircd.utils import CaseInsensitiveDictionary, now()
 
 class IRCChannel(object):
-	def __init__(self, name):
+	def __init__(self, ircd, name):
+		self.ircd = ircd
 		self.name = name
 		self.created = now()
 		self.topic = ""
@@ -12,16 +13,20 @@ class IRCChannel(object):
 		self.metadata = {}
 		self.cache = {}
 	
+	def modeString(self):
+		modes = "+"
+		params = []
+		for mode, param in self.mode.iteritems():
+			if self.ircd.channel_mode_type[mode] > 0:
+				modes += mode
+				if param:
+					params.append(param)
+		return ("{} {}".format(modes, " ".join(params)) if params else modes)
+	
 	def setTopic(self, topic, setter):
 		self.topic = topic
 		self.topicSetter = setter
 		self.topicTime = now()
-	
-	def setMode(self, modeList):
-		pass
-	
-	def unsetMode(self, modeList):
-		pass
 	
 	def getMetadata(self, key):
 		if key in self.metadata:
