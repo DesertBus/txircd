913b2d372889f5014be583fa01d5ed057ace02aa
ElementalAlchemist <ElementAlchemist7@gmail.com>
2013-02-23 21:51:33 -0500

    Implement REHASH command

---
+++ txircd/modules/cmd_rehash.py
@@ -0,0 +1,32 @@
+from twisted.words.protocols import irc
+from txircd.modbase import Command
+
+class RehashCommand(Command):
+	def onUse(self, user, data):
+		user.sendMessage(irc.RPL_REHASHING, self.ircd.config, ":Rehashing")
+		self.ircd.rehash()
+	
+	def processParams(self, user, params):
+		if user.registered > 0:
+			user.sendMessage(irc.ERR_NOTYETREGISTERED, "REHASH", ":You have not registered")
+			return {}
+		if "o" not in user.mode:
+			user.sendMessage(irc.ERR_NOPRIVILEGES, ":Permission denied - You do not have the required operator privileges")
+			return {}
+		return {
+			"user": user
+		}
+
+class Spawner(object):
+	def __init__(self, ircd):
+		self.ircd = ircd
+	
+	def spawn(self):
+		return {
+			"commands": {
+				"REHASH": RehashCommand()
+			}
+		}
+	
+	def cleanup(self):
+		del self.ircd.commands["REHASH"]
--- txircd/user.py
+++ txircd/user.py
@@ -746,13 +746,6 @@ class IRCUser(object):
 				banmask = "*@{}".format(banmask)
 			self.add_xline("SHUN", banmask, parse_duration(params[1]), " ".join(params[2:]))
 	
-	def irc_REHASH(self, prefix, params):
-		if not self.mode.has("o"):
-			self.sendMessage(irc.ERR_NOPRIVILEGES, ":Permission denied - You do not have the required operator privileges")
-			return
-		self.ircd.rehash()
-		self.sendMessage(irc.RPL_REHASHING, self.ircd.config, ":Rehashing")
-	
 	def irc_DIE(self, prefix, params):
 		if not self.mode.has("o"):
 			self.sendMessage(irc.ERR_NOPRIVILEGES, ":Permission denied - You do not have the required operator privileges")
