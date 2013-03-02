97d97380e10f7069417d68fa74176ec7fc000cc9
ElementalAlchemist <ElementAlchemist7@gmail.com>
2013-03-02 11:53:16 -0500

    Fix the name of that regex to make more sense

--- txircd/utils.py
+++ txircd/utils.py
@@ -5,7 +5,7 @@ from pbkdf2 import PBKDF2
 import re, datetime, hashlib, sys
 from base64 import b64encode, b64decode
 
-VALID_USERNAME = re.compile(r"[a-zA-Z\[\]\\`_^{}\|][a-zA-Z0-9-\[\]\\`_^{}\|]{0,31}$") # up to 32 char nicks
+VALID_NICKNAME = re.compile(r"[a-zA-Z\[\]\\`_^{}\|][a-zA-Z0-9-\[\]\\`_^{}\|]{0,31}$") # up to 32 char nicks
 DURATION_REGEX = re.compile(r"((?P<years>\d+?)y)?((?P<weeks>\d+?)w)?((?P<days>\d+?)d)?((?P<hours>\d+?)h)?((?P<minutes>\d+?)m)?((?P<seconds>\d+?)s)?")
 
 def irc_lower(str):
