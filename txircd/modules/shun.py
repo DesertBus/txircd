27ca24a93a9741cf36f36b050bdecfff2203fbb5
ElementalAlchemist <ElementAlchemist7@gmail.com>
2013-03-02 14:55:48 -0500

    Implement SHUN command

---
+++ txircd/modules/shun.py
@@ -0,0 +1,161 @@
+from twisted.words.protocols import irc
+from txircd.modbase import Command
+from txircd.utils import epoch, now, irc_lower, parse_duration, CaseInsensitiveDictionary
+from fnmatch import fnmatch
+
+irc.RPL_STATSSHUN = "223" # This use of this numeric doesn't normally have a name.
+
+class ShunCommand(Command):
+	def __init__(self):
+		self.shunList = CaseInsensitiveDictionary()
+	
+	def onUse(self, user, data):
+		if "reason" in data:
+			self.shunList[data["mask"]] = {
+				"setter": user.nickname,
+				"created": epoch(now()),
+				"duration": data["duration"],
+				"reason": data["reason"]
+			}
+		else:
+			del self.shunList[data["mask"]]
+		for udata in self.ircd.users.itervalues():
+			if self.match_shun(udata):
+				udata.cache["shunned"] = True
+			else:
+				udata.cache["shunned"] = False
+	
+	def processParams(self, user, params):
+		if user.registered > 0:
+			user.sendMessage(irc.ERR_NOTYETREGISTERED, "SHUN", ":You have not registered")
+			return {}
+		if "o" not in user.mode:
+			user.sendMessage(irc.ERR_NOPRIVILEGES, ":Permission denied - You do not have the correct operator privileges")
+			return {}
+		if not params:
+			user.sendMessage(irc.ERR_NEEDMOREPARAMS, "SHUN", ":Not enough parameters")
+			return {}
+		banmask = params[0]
+		if banmask in self.ircd.users:
+			udata = self.ircd.users[banmask]
+			banmask = "{}@{}".format(udata.username, udata.hostname)
+		elif "@" not in banmask:
+			banmask = "*@{}".format(banmask)
+		self.expire_shuns()
+		if len(params) < 3 or not params[2]:
+			if banmask not in self.shunList:
+				user.sendMessage("NOTICE", ":*** Shun {} already doesn't exist!  Check /stats S for a list of active shuns.".format(banmask))
+				return {}
+			return {
+				"user": user,
+				"mask": banmask
+			}
+		if banmask in self.shunList:
+			user.sendMessage("NOTICE", ":*** Shun {} is already set!  Check /stats S for a list of active shuns.".format(banmask))
+			return {}
+		return {
+			"user": user,
+			"mask": banmask,
+			"duration": parse_duration(params[1]),
+			"reason": " ".join(params[2:])
+		}
+	
+	def statsList(self, cmd, data):
+		if cmd != "STATS":
+			return
+		if data["statstype"] != "S":
+			return
+		self.expire_shuns()
+		for mask, linedata in self.shunList.iteritems():
+			user.sendMessage(irc.RPL_STATSSHUN, "{} {} {} {} :{}".format(mask, linedata["created"], linedata["duration"], linedata["setter"], linedata["reason"]))
+	
+	def check_register(self, user):
+		reason = self.match_shun(user)
+		if reason:
+			user.cache["shunned"] = True
+		elif reason == None:
+			user.cache["shunned"] = False
+		else:
+			return "again"
+		return True
+	
+	def reassign_shun(self, user):
+		reason = self.match_shun(user)
+		if reason:
+			user.cache["shunned"] = True
+		else:
+			user.cache["shunned"] = False
+		return None # the xline_rematch hook shouldn't automatically operate on these, so let's make it not.
+	
+	def match_shun(self, user):
+		self.expire_shuns()
+		if "except_line" in user.cache:
+			if user.cache["except_line"]:
+				return None
+			matchMask = "{}@{}".format(user.username, user.hostname)
+			for mask, linedata in self.shunList.iteritems():
+				if fnmatch(matchMask, mask):
+					return linedata["reason"]
+			matchMask = "{}@{}".format(user.username, user.ip)
+			for mask, linedata in self.shunList.iteritems():
+				if fnmatch(matchMask, mask):
+					return linedata["reason"]
+			return None
+		elif "shunned" in user.cache:
+			if user.cache["shunned"]:
+				return "Shunned"
+			return None
+		else:
+			matchMask = "{}@{}".format(user.username, user.hostname)
+			for mask in self.shunList.iterkeys():
+				if fnmatch(matchMask, mask):
+					user.cache["shunned"] = True
+					return ""
+			matchMask = "{}@{}".format(user.username, user.ip)
+			for mask in self.shunList.iterkeys():
+				if fnmatch(matchMask, mask):
+					user.cache["shunned"] = True
+					return ""
+			user.cache["shunned"] = False
+			return None
+	
+	def expire_shuns(self):
+		current_time = epoch(now())
+		expired = []
+		for mask, linedata in self.shunList.iteritems():
+			if linedata["duration"] and current_time > linedata["created"] + linedata["duration"]:
+				expired.append(mask)
+		for mask in expired:
+			del self.shunList[mask]
+	
+	def check_command(self, user, command, data):
+		if "shunned" not in user.cache or not user.cache["shunned"]:
+			return data
+		if command not in self.ircd.shun_command_list:
+			return {}
+		return data
+
+class Spawner(object):
+	def __init__(self, ircd):
+		self.ircd = ircd
+		self.shunCmd = None
+	
+	def spawn(self):
+		self.shunCmd = ShunCommand()
+		return {
+			"command": {
+				"SHUN": self.shunCmd
+			},
+			"actions": {
+				"commandextra": [self.shunCmd.statsList],
+				"register": [self.shunCmd.check_register],
+				"commandpermission": [self.shunCmd.check_command],
+				"xline_rematch": [self.shunCmd.reassign_shun]
+			}
+		}
+	
+	def cleanup(self):
+		del self.ircd.commands["SHUN"]
+		self.ircd.actions["commandextra"].remove(self.shunCmd.statsList)
+		self.ircd.actions["register"].remove(self.shunCmd.check_register)
+		self.ircd.actions["commandpermission"].remove(self.shunCmd.check_command)
