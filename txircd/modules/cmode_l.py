dd36b03f9ba982dd292abc595b9b9e2f6fb34fd0
ElementalAlchemist <ElementAlchemist7@gmail.com>
2013-03-03 10:42:20 -0500

    Implement channel mode +l

---
+++ txircd/modules/cmode_l.py
@@ -0,0 +1,42 @@
+from twisted.words.protocols import irc
+from txircd.modbase import Mode
+
+class LimitMode(Mode):
+	def checkSet(self, user, target, param):
+		intParam = int(param)
+		if str(intParam) != param:
+			return False
+		return (intParam >= 0)
+	
+	def commandPermission(self, user, cmd, data):
+		if cmd != "JOIN":
+			return data
+		targetChannels = data["targetchan"]
+		keys = data["keys"]
+		removeChannels = []
+		for channel in targetChannels:
+			if "l" in channel.mode and len(channel.users) >= int(channel.mode["l"]):
+				user.sendMessage(irc.ERR_CHANNELISFULL, channel.name, ":Cannot join channel (Channel is full)")
+				removeChannels.append(channel)
+		
+		for channel in removeChannels:
+			index = targetChannels.index(channel)
+			targetChannels.pop(index)
+			keys.pop(index)
+		data["targetchan"] = targetChannels
+		data["keys"] = keys
+		return data
+
+class Spawner(object):
+	def __init__(self, ircd):
+		self.ircd = ircd
+	
+	def spawn(self):
+		return {
+			"modes": {
+				"cpl": LimitMode()
+			}
+		}
+	
+	def cleanup(self):
+		self.ircd.removeMode("cpl")
