b1f3a3f03e86ecebe6225ba6763d4d9f8feac469
ElementalAlchemist <ElementAlchemist7@gmail.com>
2013-05-31 22:17:21 -0400

    Be more polite to users who give us invalid AES data

--- txircd/modules/db_services.py
+++ txircd/modules/db_services.py
@@ -1461,7 +1461,10 @@ class Spawner(object):
         sharedSecret = self.binaryString(pow(pubkey, self.dh_params["privkey"], self.dh_params["prime"]))
         
         aesCipher = AES.new(sharedSecret, mode=AES.MODE_CBC, IV=iv)
-        decryptedData = aesCipher.decrypt(encryptedData)
+        try:
+            decryptedData = aesCipher.decrypt(encryptedData)
+        except ValueError:
+            return "done"
         
         try:
             username, password, padding = decryptedData.split("\0", 2)
