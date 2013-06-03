df8285a588b729b76f843d5a899a111cfcc590ca
ElementalAlchemist <ElementAlchemist7@gmail.com>
2013-06-02 23:31:34 -0400

    Take burst data and merge it with our own server data

--- txircd/server.py
+++ txircd/server.py
@@ -246,16 +246,41 @@ class ServerProtocol(AMP):
             raise BurstIncomplete ("The handshake was not completed before attempting to burst data.")
         if "burst-recv" in self.burstStatus:
             raise AlreadyBursted ("Data has already been bursted to this server.")
+        incomingChannels = []
+        for chan in channels:
+            newChannel = IRCChannel(self.ircd, chan["name"])
+            newChannel.created = datetime.utcfromtimestamp(chan["ts"])
+            newChannel.topic = chan["topic"]
+            newChannel.topicSetter = chan["topicsetter"]
+            newChannel.topicTime = datetime.utcfromtimestamp(chan["topicts"])
+            newChannel.cache["mergingusers"] = chan["users"]
+            for mode in chan["mode"]:
+                modetype = self.ircd.channel_mode_type[mode[0]]
+                if modetype == 0:
+                    if mode[0] not in newChannel.mode:
+                        newChannel.mode[mode[0]] = []
+                    newChannel.mode[mode[0]].append(mode[1:])
+                elif modetype == 3:
+                    newChannel.mode[mode[0]] = None
+                else:
+                    newChannel.mode[mode[0]] = mode[1:]
+            incomingChannels.append(newChannel)
         for udata in users:
             if udata["nickname"] in self.ircd.users: # a user with the same nickname is already connected
                 ourudata = self.ircd.users[udata["nickname"]]
                 ourts = epoch(ourudata.nicktime)
                 if ourts == udata["ts"]: # older user wins; if same, they both die
                     ourudata.disconnect("Nickname collision")
+                    for channel in incomingChannels:
+                        if udata["nickname"] in channel.cache["mergingusers"]:
+                            channel.cache["mergingusers"].remove(udata["nickname"])
                     continue
                 elif ourts > udata["ts"]:
                     ourudata.disconnect("Nickname collision")
                 else:
+                    for channel in incomingChannels:
+                        if udata["nickname"] in channel.cache["mergingusers"]:
+                            channel.cache["mergingusers"].remove(udata["nickname"])
                     continue # skip adding the remote user since they'll die on the remote server
             newUser = RemoteUser(self.ircd, udata["nickname"], udata["ident"], udata["host"], udata["gecos"], udata["ip"], self.name, udata["secure"], datetime.utcfromtimestamp(udata["signon"]), datetime.utcfromtimestamp(udata["ts"]))
             for mode in udata["mode"]:
@@ -268,9 +293,163 @@ class ServerProtocol(AMP):
                     newUser.mode[mode[0]] = None
                 else:
                     newUser.mode[mode[0]] = mode[1:]
-            
+            for chan in udata["channels"]:
+                newUser.channels[chan["name"]] = { "status": chan["status"] } # This will get fixed in the channel merging to immediately follow
+            self.ircd.users[udata["nickname"]] = newUser
+        for channel in incomingChannels:
+            for user in channel.cache["mergingusers"]:
+                channel.users.add(self.ircd.users[user])
+            del channel.cache["mergingusers"]
+            if channel.name not in self.ircd.channels:
+                self.ircd.channels[channel.name] = channel # simply add the channel to our list
+            else:
+                mergeChanData = self.ircd.channels[channel.name]
+                if channel.created == mergeChanData.created: # ... matching timestamps? Time to resolve lots of conflicts
+                    if channel.topicTime >= mergeChanData.topicTime:
+                        # topics: if identical contents and setter but different timestamps, keep older timestamp
+                        # if different topics, keep newer topic
+                        if channel.topic == mergeChanData.topic and channel.topicSetter == mergeChanData.topicSetter:
+                            channel.topicTime = mergeChanData.topicTime # If the topics are identical, go with the lower timestamp
+                        else:
+                            mergeChanData.setTopic(channel.topic, channel.topicSetter)
+                            mergeChanData.topicTime = channel.topicTime
+                            for user in mergeChanData.users:
+                                user.sendMessage("TOPIC", ":{}".format(channel.topic), to=mergeChanData.name)
+                    # modes: merge modes together
+                    # break parameter ties on normal parameter modes by giving the winner to the server being connected to
+                    modeDisplay = []
+                    paramDisplay = []
+                    for mode, param in channel.mode.iteritems():
+                        modetype = self.ircd.channel_mode_type[mode]
+                        if modetype == 0:
+                            for item in param:
+                                if item not in mergeChanData.mode[mode]:
+                                    mergeChanData.mode[mode].append(item)
+                                    modeDisplay.append(mode)
+                                    paramDisplay.append(param)
+                        elif modetype == 3:
+                            if mode not in mergeChanData.mode:
+                                mergeChanData.mode[mode] = None
+                                modeDisplay.append(mode)
+                        else:
+                            if mode not in mergeChanData.mode:
+                                mergeChanData.mode[mode] = param
+                                modeDisplay.append(mode)
+                                paramDisplay.append(param)
+                            elif self.localOrigin:
+                                mergeChanData.mode[mode] = param
+                                modeDisplay.append(mode)
+                                paramDisplay.append(param)
+                    if modeDisplay:
+                        if paramDisplay:
+                            for user in mergeChanData.users:
+                                user.sendMessage("MODE", "+{} {}".format("".join(modeDisplay), " ".join(paramDisplay)), to=mergeChanData.name)
+                        else:
+                            for user in mergeChanData.users:
+                                user.sendMessage("MODE", "+{}".format("".join(modeDisplay)), to=mergeChanData.name)
+                    # channel lists are already fine, just notify users
+                    for user in channel.users:
+                        self.justSendJoin(user, mergeChanData)
+                    for user in channel.users: # Run this as a separate loop so that remote users don't get repeat join messages for users already in that channel
+                        mergeChanData.users.add(user)
+                elif channel.created < mergeChanData.created: # theirs is older, so discard any changes ours made
+                    # Topic: If the contents and setter are the same, adopt the remote timestamp; otherwise, adopt the
+                    # remote topic and alert users of the change
+                    if channel.topic == mergeChanData.topic and channel.topicSetter == mergeChanData.topicSetter:
+                        mergeChanData.topicTime = channel.topicTime
+                    else:
+                        mergeChanData.setTopic(channel.topic, channel.topicSetter)
+                        mergeChanData.topicTime = channel.topicTime
+                        for user in mergeChanData.users:
+                            user.sendMessage("TOPIC", ":{}".format(channel.topic), to=mergeChanData.name)
+                    # Modes: Take modes of remote side; keep track of mode changes to alert our users
+                    modeDisplay = []
+                    removeModes = []
+                    for mode, param in mergeChanData.mode.iteritems():
+                        modetype = self.ircd.channel_mode_type[mode]
+                        if modetype == 0:
+                            listData = param
+                            for item in listData:
+                                if mode not in channel.mode or item not in channel.mode[mode]:
+                                    param.remove(item)
+                                    modeDisplay.append([False, mode, param])
+                        elif modetype == 3:
+                            if mode not in channel.mode:
+                                removeModes.append(mode)
+                                modeDisplay.append([False, mode, None])
+                        else:
+                            if mode not in channel.mode:
+                                removeModes.append(mode)
+                                modeDisplay.append([False, mode, param])
+                    for mode in removeModes:
+                        del mergeChanData.mode[mode]
+                    for mode, param in channel.mode.iteritems():
+                        modetype = self.ircd.channel_mode_type[mode]
+                        if modetype == 0:
+                            mergeChanData.mode[mode].append(param)
+                            modeDisplay.append([True, mode, param])
+                        else:
+                            mergeChanData.mode[mode] = param
+                            modeDisplay.append([True, mode, param])
+                    # Users (local): remove statuses, send mode change notice
+                    for user in mergeChanData.users:
+                        statuses = user.channels[mergeChanData.name]["status"]
+                        user.channels[mergeChanData.name]["status"] = ""
+                        for status in statuses:
+                            modeDisplay.append([False, status, user.nickname])
+                    # Users (remote): show join and statuses of new users
+                    for user in channel.users:
+                        statuses = user.channels[channel.name]["status"]
+                        for status in statuses:
+                            modeDisplay.append([True, status, user.nickname])
+                        self.justSendJoin(user, mergeChanData)
+                    for user in channel.users: # Run as a second loop so remote users don't get another JOIN message about users already in that channel
+                        mergeChanData.users.add(user)
+                    # Send all the modes we should be sending
+                    if modeDisplay:
+                        modeStr = []
+                        params = []
+                        adding = None
+                        for mode in modeDisplay:
+                            if mode[0] and adding is not True:
+                                modeStr.append("+")
+                            elif not mode[0] and adding is not False:
+                                modeStr.append("-")
+                            modeStr.append(mode[1])
+                            if mode[2]:
+                                params.append(mode[2])
+                        if params:
+                            for user in mergeChanData.users:
+                                user.sendMessage("MODE", "{} {}".format("".join(modeStr), " ".join(params)), to=mergeChanData.name)
+                        else:
+                            for user in mergeChanData.users:
+                                user.sendMessage("MODE", "".join(modeStr), to=mergeChanData.name)
+                else: # ours is older, so discard any changes theirs made
+                    # topic: keep ours; ignore theirs
+                    # modes: keep ours; ignore theirs
+                    # users (local): keep same
+                    # users (remote): remove status, add to channel
+                    for user in channel.users:
+                        user.channels[channel.name]["status"] = ""
+                        self.justSendJoin(user, mergeChanData)
+                    for user in channel.users: # Use a second loop so remote users don't get extra JOIN messages about users already in that channel
+                        mergeChanData.users.add(user)
         return {}
     BurstData.responder(burstData)
+    
+    def justSendJoin(self, user, channel):
+        joinShowUsers = set(mergeChanData.users) # copy the channel.users set to prevent accidental modification of the users list
+        tryagain = []
+        for modfunc in self.ircd.actions["joinmessage"]:
+            result = modfunc(channel, user, joinShowUsers)
+            if result == "again":
+                tryagain.append(modfunc)
+            else:
+                joinShowUsers = result
+        for modfunc in tryagain:
+            joinShowUsers = modfunc(channel, user, joinShowUsers)
+        for u in joinShowUsers:
+            u.sendMessage("JOIN", to=channel.name, prefix=user.prefix())
     
     def sendBurstData(self):
         if "burst-send" in self.burstStatus:
