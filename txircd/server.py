6b1476cc0e6c706e6666a602d7fe0657ffc0b71d
ElementalAlchemist <ElementAlchemist7@gmail.com>
2013-07-05 20:31:26 -0400

    Try setting the name before sending burst data that relies on it

--- txircd/server.py
+++ txircd/server.py
@@ -455,10 +455,10 @@ class ServerProtocol(AMP):
         if self.sentDataBurst is None:
             self.callRemote(IntroduceServer, name=self.ircd.name, password=linkData["outgoing_password"], description=self.ircd.servconfig["server_description"], version=protocol_version, commonmodules=self.ircd.common_modules)
             self.sentDataBurst = False
-        self.sendBurstData()
         self.name = name
         self.description = description
         self.ircd.servers[self.name] = self
+        self.sendBurstData()
         return {}
     IntroduceServer.responder(serverHandshake)
     
